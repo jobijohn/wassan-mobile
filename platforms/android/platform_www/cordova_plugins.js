a2646d7d6f526d6148c7e8fc88c9969d5280f0b8
jobijohn <jobikjohn@gmail.com>
2016-10-28 14:05:54 +0530

    mobile project initialized

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,36 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "pluginId": "com.brodysoft.sqlitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "id": "org.apache.cordova.dialogs.notification",
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "pluginId": "org.apache.cordova.dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "pluginId": "org.apache.cordova.dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.brodysoft.sqlitePlugin": "1.0.3",
+    "org.apache.cordova.console": "0.2.12",
+    "org.apache.cordova.dialogs": "0.2.11"
+};
+// BOTTOM OF METADATA
+});
